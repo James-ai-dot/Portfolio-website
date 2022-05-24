e25fc79fe79dc49d9d30ec21149194e274b63e42
James-ai-dot <105008489+James-ai-dot@users.noreply.github.com>
2022-05-24 15:34:48 +0200

    create hamburger menu mobile screens

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,20 @@
-console.log("Hi! Welcome to my portfolio site")
+function menuToggle() {
+}
+
+function menuToggle() {
+    var x = document.getElementById('myNavtoggle');
+  }
+
+function menuToggle() {
+    var x = document.getElementById('myNavtoggle');
+    x.className += ' responsive';
+  }
+
+  function menuToggle() {
+    var x = document.getElementById('myNavtoggle');
+    if (x.className === 'navtoggle') {
+      x.className += ' responsive';
+    } else {
+      x.className = 'navtoggle';
+    }
+  }
