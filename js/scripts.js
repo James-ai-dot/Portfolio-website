9e2e4658666102e314b83935e23dc9f64fb296c4
James-ai-dot <105008489+James-ai-dot@users.noreply.github.com>
2022-05-24 15:38:16 +0200

    fix js.script for hamburger icon

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,4 @@
 function menuToggle() {
-}
-
-function menuToggle() {
-    var x = document.getElementById('myNavtoggle');
-  }
-
-function menuToggle() {
-    var x = document.getElementById('myNavtoggle');
-    x.className += ' responsive';
-  }
-
-  function menuToggle() {
     var x = document.getElementById('myNavtoggle');
     if (x.className === 'navtoggle') {
       x.className += ' responsive';
