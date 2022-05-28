7b046df7c084d0832264ffe0a0088e1e0ad5c70d
James-ai-dot <105008489+James-ai-dot@users.noreply.github.com>
2022-05-28 12:05:10 +0200

    update

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
+// Mobile navigation
+
 function menuToggle() {
     var x = document.getElementById('myNavtoggle');
     if (x.className === 'navtoggle') {
@@ -6,3 +8,17 @@ function menuToggle() {
       x.className = 'navtoggle';
     }
   }
+
+// ===== Scroll to Top ==== 
+$(window).scroll(function() {
+  if ($(this).scrollTop() >= 50) {        // If page is scrolled more than 50px
+      $('#return-to-top').fadeIn(200);    // Fade in the arrow
+  } else {
+      $('#return-to-top').fadeOut(200);   // Else fade out the arrow
+  }
+});
+$('#return-to-top').click(function() {      // When arrow is clicked
+  $('body,html').animate({
+      scrollTop : 0                       // Scroll to top of body
+  }, 500);
+});
