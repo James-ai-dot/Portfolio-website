fa536e3b8827a88abdfccd15035c778b5fdf368b
James-ai-dot <105008489+James-ai-dot@users.noreply.github.com>
2022-05-30 15:10:04 +0200

    update vintage life

--- js/scripts.js
+++ js/scripts.js
@@ -22,3 +22,25 @@ $('#return-to-top').click(function() {      // When arrow is clicked
       scrollTop : 0                       // Scroll to top of body
   }, 500);
 });
+
+// Image modal
+
+// Modal Setup
+var modal = document.getElementById('modal');
+
+var modalClose = document.getElementById('modal-close');
+modalClose.addEventListener('click', function() { 
+  modal.style.display = "none";
+});
+
+// global handler
+document.addEventListener('click', function (e) { 
+  if (e.target.className.indexOf('modal-target') !== -1) {
+      var img = e.target;
+      var modalImg = document.getElementById("modal-content");
+      var captionText = document.getElementById("modal-caption");
+      modal.style.display = "block";
+      modalImg.src = img.src;
+      captionText.innerHTML = img.alt;
+   }
+});
