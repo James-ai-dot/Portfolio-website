49b2fd28a0159c44a49bbb42b6586e77634ccb0a
James-ai-dot <105008489+James-ai-dot@users.noreply.github.com>
2022-06-09 22:30:48 +0200

    update prototype

--- js/scripts.js
+++ js/scripts.js
@@ -9,53 +9,9 @@ function menuToggle() {
     }
   }
 
-// ===== Scroll to Top ==== 
-$(window).scroll(function() {
-  if ($(this).scrollTop() >= 50) {        // If page is scrolled more than 50px
-      $('#return-to-top').fadeIn(200);    // Fade in the arrow
-  } else {
-      $('#return-to-top').fadeOut(200);   // Else fade out the arrow
-  }
+//  Get the button
+returnTop = document.getElementById("return-top")
+
+returnTop.addEventListener("click", function() {
+  window.scrollTo(0, 0);
 });
-$('#return-to-top').click(function() {      // When arrow is clicked
-  $('body,html').animate({
-      scrollTop : 0                       // Scroll to top of body
-  }, 500);
-});
-
-// Image modal
-
-// Modal Setup
-var modal = document.getElementById('modal');
-
-var modalClose = document.getElementById('modal-close');
-modalClose.addEventListener('click', function() { 
-  modal.style.display = "none";
-});
-
-// global handler
-document.addEventListener('click', function (e) { 
-  if (e.target.className.indexOf('modal-target') !== -1) {
-      var img = e.target;
-      var modalImg = document.getElementById("modal-content");
-      var captionText = document.getElementById("modal-caption");
-      modal.style.display = "block";
-      modalImg.src = img.src;
-      captionText.innerHTML = img.alt;
-   }
-});
-
-// Process model
-
-// script for tab steps
-$('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
-
-  var href = $(e.target).attr('href');
-  var $curr = $(".process-model  a[href='" + href + "']").parent();
-
-  $('.process-model li').removeClass();
-
-  $curr.addClass("active");
-  $curr.prevAll().addClass("visited");
-});
-// end  script for tab steps
