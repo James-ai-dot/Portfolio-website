6db0231de8e37b1cc2e2a233abe1d5a4dd332722
James-ai-dot <105008489+James-ai-dot@users.noreply.github.com>
2022-06-09 23:54:40 +0200

    update js

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,17 @@
 // Mobile navigation
 
 function menuToggle() {
-    var x = document.getElementById('myNavtoggle');
-    if (x.className === 'navtoggle') {
-      x.className += ' responsive';
-    } else {
-      x.className = 'navtoggle';
-    }
+  var x = document.getElementById("myNavtoggle");
+  if (x.className === "navtoggle") {
+    x.className += " responsive";
+  } else {
+    x.className = "navtoggle";
   }
+}
 
 //  Get the button
-returnTop = document.getElementById("return-top")
+returnTop = document.getElementById("return-top");
 
-returnTop.addEventListener("click", function() {
+returnTop.addEventListener("click", function () {
   window.scrollTo(0, 0);
 });
