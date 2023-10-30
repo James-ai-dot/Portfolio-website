6517bcb9d3b88f402f4826d63b47abcb81230ece
James-ai-dot <james.david.turrell@gmail.com>
2023-10-30 17:33:30 +0100

    Added stagger to animation scroll

--- js/scroll.js
+++ js/scroll.js
@@ -1,11 +1,30 @@
-const observer = new IntersectionObserver((entries) => {
-  entries.forEach((entry) => {
-    console.log(entry);
-    if (entry.isIntersecting) {
-      entry.target.classList.add("show");
-    }
-  });
-});
+const sections = document.querySelectorAll("section");
 
-const hiddenElements = document.querySelectorAll(".hidden");
-hiddenElements.forEach((el) => observer.observe(el));
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        staggerShow(entry.target);
+      }
+    });
+  },
+  { threshold: 0.1 }
+);
+
+let cumulativeDelay = 0; // To keep track of total delay
+
+function staggerShow(container) {
+  const hiddenElements = container.querySelectorAll(".hidden");
+  hiddenElements.forEach((el, index) => {
+    setTimeout(() => {
+      el.classList.add("show");
+      el.classList.remove("hidden");
+    }, cumulativeDelay + index * 100);
+  });
+
+  cumulativeDelay += hiddenElements.length * 100; // Increase delay for next section
+}
+
+sections.forEach((section) => {
+  observer.observe(section);
+});
